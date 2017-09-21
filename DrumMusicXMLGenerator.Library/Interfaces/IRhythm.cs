894ccc0661959c9cbdeb6baea77b2a8c5ca4c5b0
HuePurkett <hue@wavecable.com>
2017-09-21 14:52:58 -0700

    Added interface files.

---
+++ DrumMusicXMLGenerator.Library/Interfaces/IRhythm.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace SnareDrumNotator.Library.Interfaces
+{
+    /// <summary>
+    /// Interface that represents a single note.
+    /// </summary>
+    public interface IRhythm
+    {
+        XElement GetXml();
+
+        bool IsFlam();
+
+        bool IsFlatFlam();
+
+        bool IsTupletStart();
+
+        bool IsTupletEnd();
+
+        int GetVal();
+
+        int GetLength();
+    }
+}
---
+++ DrumMusicXMLGenerator.Library/Interfaces/IRudiment.cs
@@ -0,0 +1,11 @@
+﻿namespace DrumMusicXMLGenerator.Library.Interfaces
+{
+    /// <summary>
+    /// Interface that represents a rudiment. 
+    /// Decorator of <see cref="IRhythm"/>
+    /// </summary>
+    public interface IRudiment:IRhythm
+    {
+        IRhythm SubRhythm { get; set; }
+    }
+}
